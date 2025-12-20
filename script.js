069712cb584840faa72aab77990801c1eaa8d092
joshuahohaia <joshua.hohaia@gmail.com>
2025-12-20 11:17:26 +0000

    (chore): Update timeline items to show initial details and indicate if dropped down

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function toggleCrt() {
     localStorage.setItem('crt', isCrtOn ? 'on' : 'off');
 }
 
-if (localStorage.getItem('crt') !== 'off') {
+if (localStorage.getItem('crt') === 'on') {
     document.body.classList.add('crt');
     crtToggleButton.classList.add('active');
 }
@@ -69,51 +69,13 @@ const timelineItems = document.querySelectorAll('.timeline-item-header');
 timelineItems.forEach(item => {
     item.addEventListener('click', () => {
         const body = item.nextElementSibling;
-        
+        item.classList.toggle('open');
+
         // Toggle the clicked item's body
         if (body.style.maxHeight) {
             body.style.maxHeight = null;
         } else {
             body.style.maxHeight = body.scrollHeight + "px";
-        }
-
-        // Find the parent timeline body, if it exists
-        const parentBody = item.closest('.timeline-item-body');
-
-        // If we are in a sub-timeline and the parent is open, update its height
-        if (parentBody && parentBody.style.maxHeight) {
-            // Use a timeout to allow the child animation to start and for the DOM to update
-            setTimeout(() => {
-                parentBody.style.maxHeight = parentBody.scrollHeight + "px";
-            }, 300); // 300ms matches the CSS transition time
-        }
-    });
-});
-
-const seeMoreLinks = document.querySelectorAll('.see-more');
-
-seeMoreLinks.forEach(link => {
-    link.addEventListener('click', (e) => {
-        e.stopPropagation(); 
-
-        const parentBody = link.closest('.timeline-item-body');
-        const details = parentBody.querySelector('.experience-details');
-
-        if (details) {
-            // Check if the details are already shown
-            if (details.style.maxHeight) {
-                details.style.maxHeight = null;
-            } else {
-                details.style.maxHeight = details.scrollHeight + "px";
-                link.style.display = 'none'; // Hide the link only after expanding
-            }
-
-            // Recalculate parent max-height
-            if (parentBody.style.maxHeight) {
-                setTimeout(() => {
-                    parentBody.style.maxHeight = parentBody.scrollHeight + "px";
-                }, 300); // Match CSS transition
-            }
         }
     });
 });
